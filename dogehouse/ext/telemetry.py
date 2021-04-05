4db23c80d94fdbf763c9ca2a1764b444155449e4
Arthurdw <mail@arthurdw.com>
2021-04-05 11:13:18 +0200

    ✨ Improvement for Crispy

--- dogehouse/ext/telemetry.py
+++ dogehouse/ext/telemetry.py
@@ -24,6 +24,7 @@ async def transmit(client):
     
     await socket.emit("transmit", dumps({
         "bot": {
+            "type": "dogehouse.py",
             "uuid": client.user.id,
             "username": client.user.username,
             "avatarURL": client.user.avatar_url 
