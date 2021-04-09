e7197e48e99f958e06cd42a55eab86dea3c0665f
Arthurdw <mail@arthurdw.com>
2021-04-09 13:16:54 +0200

    ✨ Did not write the ,

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha'
-        'Development Status :: 4 - Beta'
+        'Development Status :: 4 - Beta',
         # Development Status :: 5 - Production/Stable
         # Development Status :: 6 - Mature
         # Development Status :: 7 - Inactive
