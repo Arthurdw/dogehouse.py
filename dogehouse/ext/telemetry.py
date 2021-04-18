1a7709b511338edb7a31979a409cf96cb32dc127
Arthurdw <mail@arthurdw.com>
2021-04-18 12:46:10 +0200

    ✨ Preps for telemetry patch

--- dogehouse/ext/telemetry.py
+++ dogehouse/ext/telemetry.py
@@ -1,5 +1,6 @@
 try:
     from socketio import AsyncClient
+    from socketio.exceptions import ConnectionError
 except (ImportError, ModuleNotFoundError):
     raise ImportError(
         "To use telemetry you must install the `telemetry` option. (run `pip install -U dogehouse[telemetry]`)")
@@ -8,6 +9,7 @@ from json import dumps
 
 socket = AsyncClient()
 activated = False
+err_occurred_on_connect = False
 
 
 @socket.event
@@ -15,11 +17,13 @@ async def connect():
     global activated
 
     activated = True
-    await socket.emit("init")
 
 
 async def transmit(client):
-    global activated
+    global activated, err_occurred_on_connect
+
+    if err_occurred_on_connect:
+        return await start()
 
     if not activated or not hasattr(client.user, "id"):
         return
@@ -41,7 +45,7 @@ async def transmit(client):
                     "bio": user.bio if hasattr(user, "bio") else None,
                     "avatarUrl": user.avatar_url if hasattr(user, "avatar_url") else None,
                     "username": user.username if hasattr(user, "username") else None,
-                    "displayname": user.displayname,
+                    "displayName": user.displayname,
                     "numFollowers": user.num_followers,
                     "numFollowing": user.num_following if hasattr(user, "num_following") else 0,
                 } for user in client.room.users
@@ -51,8 +55,13 @@ async def transmit(client):
 
 
 async def start():
-    await socket.connect("wss://socket.dogegarden.net", transports=["websocket"], socketio_path="/socket")
-    await socket.wait()
+    global err_occurred_on_connect
+    try:
+        await socket.connect("wss://socket.dogegarden.net", transports=["websocket"], socketio_path="/socket")
+        err_occurred_on_connect = False
+        await socket.wait()
+    except ConnectionError:
+        err_occurred_on_connect = True
 
 
 class Dogegarden:
